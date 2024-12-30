84773a319440cffb82f18274394793f1f3277929
ahmedalgrgawy <ahmedalgrgawy77@gmail.com>
2024-12-30 20:06:44 +0200

    Deploy

--- Bloggly/server/server.js
+++ Bloggly/server/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors"
+import path from "path";
+
 import authRoutes from './routes/auth.routes.js'
 import blogRoutes from './routes/blog.routes.js'
 
@@ -12,6 +14,8 @@ dotenv.config();
 const app = express()
 
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 
 app.use(express.json({ limit: "50mb" }));
 
@@ -26,6 +30,15 @@ app.use(
 
 app.use('/api/user', authRoutes)
 app.use("/api/blog", blogRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'))
+    })
+}
+
 
 app.listen(port, () => {
     connectDB()
